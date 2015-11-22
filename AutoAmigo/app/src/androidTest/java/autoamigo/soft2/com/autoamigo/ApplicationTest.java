5dbcda1945a0d401ca189f000a3981a3d5f38e64
ValeriaCDL <Valeria@Valerias-MacBook-Air.local>
2015-11-22 10:58:57 -0800

    Primer prueba
    Probando que el mapa se despliegue

---
+++ AutoAmigo/app/src/androidTest/java/autoamigo/soft2/com/autoamigo/ApplicationTest.java
@@ -0,0 +1,13 @@
+package autoamigo.soft2.com.autoamigo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
